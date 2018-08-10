fb0a06b25c0d774f7111dacc61987c67c4a9f5b3
norman jaya subrata <normanjayasubrata@normans-MacBook-Pro.local>
2018-08-10 10:12:18 +0700

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "../node_modules/materialize-css/dist/css/materialize.min.css";
-import "../node_modules/materialize-css/dist/js/materialize.min.js";
-
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
